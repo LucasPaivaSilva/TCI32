1e33825f18db10c2b3e5b6d3af58827183262964
Paiva-Yoga730 <37738982+LucasPaivaSilva@users.noreply.github.com>
2021-06-05 16:58:58 -0300

    added basic display functions
    boot screen
    MIDI Mode screen (basic)

--- TCI_Firmware/src/main.cpp
+++ TCI_Firmware/src/main.cpp
@@ -1,5 +1,12 @@
 #include <Arduino.h>
 #include <BLEMidi.h>
+#include <Wire.h>
+#include <Adafruit_I2CDevice.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(128 , 64, &Wire, -1);
 
 volatile int interruptCounter;
 hw_timer_t * timer = NULL;
@@ -10,24 +17,35 @@ unsigned long note1freq;
 unsigned char note1_duty;
 uint8_t note1_note;
 bool note1on = false;
+int note1toDisplay = 0;
 
 unsigned long bres2;       
 unsigned long note2freq;   
 unsigned char note2_duty;
 uint8_t note2_note;
 bool note2on = false;
+int note2toDisplay = 0;
 
 unsigned long bres3;       
 unsigned long note3freq;   
 unsigned char note3_duty;
 uint8_t note3_note;
 bool note3on = false;
+int note3toDisplay = 0;
 
 void connected();
+void SetupMidi();
+void SetupInterrupts();
+void SetupDisplay();
 
 int PitchToFreq(int pitch)
 {	
-	return (int) (220.0 * pow(pow(2.0, 1.0/12.0), pitch - 57) + 0.5);
+  int freq = (int) (220.0 * pow(pow(2.0, 1.0/12.0), pitch - 57) + 0.5);
+  if (freq>999)
+  {
+    return 999;
+  }
+	return freq;
 }
 
 void onNoteOn(uint8_t channel, uint8_t note, uint8_t velocity, uint16_t timestamp)
@@ -38,18 +56,21 @@ void onNoteOn(uint8_t channel, uint8_t note, uint8_t velocity, uint16_t timestam
     note1on = true;
     note1freq = PitchToFreq(note);
     note1_note = note;
+    note1toDisplay = note1freq;
   }
   else if (note2on == false)
   {
     note2on = true;
     note2freq = PitchToFreq(note); 
     note2_note = note;
+    note2toDisplay = note2freq;
   }
   else if (note3on == false)
   {
     note3on = true;
     note3freq = PitchToFreq(note); 
     note3_note = note;
+    note3toDisplay = note3freq;
   }
 }
 
@@ -59,14 +80,17 @@ void onNoteOff(uint8_t channel, uint8_t note, uint8_t velocity, uint16_t timesta
   if (note == note1_note)
   {
     note1on = false;
+    //note1toDisplay = 0;
   }
   if (note == note2_note)
   {
     note2on = false;
+    //note2toDisplay = 0;
   }
   if (note == note3_note)
   {
     note3on = false;
+    //note3toDisplay = 0;
   }
 }
 
@@ -78,6 +102,18 @@ void onControlChange(uint8_t channel, uint8_t controller, uint8_t value, uint16_
 void connected()
 {
   Serial.println("Connected");
+}
+
+void SetupMidi()
+{
+  BLEMidiServer.begin("TCI_MIDI");
+  BLEMidiServer.setOnConnectCallback(connected);
+  BLEMidiServer.setOnDisconnectCallback([](){     // To show how to make a callback with a lambda function
+  Serial.println("Disconnected");
+  });
+  BLEMidiServer.setNoteOnCallback(onNoteOn);
+  BLEMidiServer.setNoteOffCallback(onNoteOff);
+  BLEMidiServer.setControlChangeCallback(onControlChange);
 }
 
 void IRAM_ATTR onTimer() {
@@ -134,30 +170,111 @@ void IRAM_ATTR onTimer() {
  
 }
 
-void setup() {
-  pinMode(2, OUTPUT);
-  digitalWrite(2, LOW);
-  delay(1000);
-  Serial.begin(115200);
+void SetupInterrupts()
+{
   timer = timerBegin(0, 8, true);
   timerAttachInterrupt(timer, &onTimer, true);
   timerAlarmWrite(timer, 100, true);
   timerAlarmEnable(timer);
+}
+
+void UpdateMIDIMode(bool DrawEntireDisplay, bool UptadeInfo)
+{
+  if (DrawEntireDisplay)
+  {
+    display.clearDisplay();
+    display.setTextSize(1);            
+    display.setTextColor(SSD1306_WHITE);       
+    display.setCursor(0,0);            
+    display.println(F("TCI 0.5"));
+    display.println(F(""));
+    display.println(F("Note1 > Off 000 Hz"));
+    display.println(F("Note2 > Off 000 Hz"));
+    display.println(F("Note3 > Off 000 Hz"));
+    display.display();
+  }
+  if (UptadeInfo)
+  {
+    display.clearDisplay();
+    display.setTextSize(1);            
+    display.setTextColor(SSD1306_WHITE);       
+    display.setCursor(0,0);            
+    display.println(F("TCI 0.5"));
+    display.println(F(""));
+
+    display.print(F("Note1 > "));
+    if (note1on)
+    {
+      display.print(F("On  "));
+    }
+    else
+    {
+      display.print(F("Off "));
+    }
+    display.print((note1toDisplay));
+    display.println(F(" Hz"));
+
+    display.print(F("Note2 > "));
+    if (note2on)
+    {
+      display.print(F("On  "));
+    }
+    else
+    {
+      display.print(F("Off "));
+    }
+    display.print((note2toDisplay));
+    display.println(F(" Hz"));
+
+    display.print(F("Note3 > "));
+    if (note3on)
+    {
+      display.print(F("On  "));
+    }
+    else
+    {
+      display.print(F("Off "));
+    }
+    display.print((note3toDisplay));
+    display.println(F(" Hz"));
+    display.display();
+  }
+
+  
+}
+
+void SetupDisplay()
+{
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.clearDisplay();
+  display.setTextSize(2);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F(""));
+  display.println(F("TCI 0.5"));
+  display.println(F("By Paiva"));
+  display.display();
+}
+
+void setup() {
+  pinMode(2, OUTPUT);
+  digitalWrite(2, LOW);
+  Serial.begin(115200);
+  SetupDisplay();
+  SetupInterrupts();
+  SetupMidi();
   Serial.println("Sistema iniciado");
-  BLEMidiServer.begin("MIDI device");
-  BLEMidiServer.setOnConnectCallback(connected);
-  BLEMidiServer.setOnDisconnectCallback([](){     // To show how to make a callback with a lambda function
-  Serial.println("Disconnected");
-  });
-  BLEMidiServer.setNoteOnCallback(onNoteOn);
-  BLEMidiServer.setNoteOffCallback(onNoteOff);
-  BLEMidiServer.setControlChangeCallback(onControlChange);
-  //BLEMidiServer.enableDebugging();
+  delay(1000);
+  UpdateMIDIMode(true, false);
 }
 
 void loop() {
-  if (BLEMidiServer.isConnected()) {
-      
-  }
-  delay(2000);
+  delay(100);
+  UpdateMIDIMode(false, true);
 }
